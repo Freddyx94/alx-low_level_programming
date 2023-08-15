3f8237ae5e01d982cdb37302854430e9d2dcde63
Freddyx94 <m.saa94@yahoo.com>
2023-08-15 06:02:31 -0700

    futher corrections on task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,20 +7,21 @@
 
 void print_alphabet_x10(void)
 {
+
 	char ch;
+
 	int i;
 
-	i = 10;
+	i = 0;
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
-			ch++;
 		}
 		_putchar('\n');
 		i++;
 	}
+
 }
