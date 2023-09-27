e93b49161ee347f3873cadc5e925813eaf44bc4a
Freddyx94 <lilfreddie9@hotmail.com>
2023-09-27 14:19:31 +0100

    Corrections to task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t format = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	for (format = 0; h != NULL; format++)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
-
-	return (num);
+	return (format);
 }
